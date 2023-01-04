7048140cb9ce050bc66fa58ba235a086055cad8a
Sun-mie <104572619+Sun-mie@users.noreply.github.com>
2023-01-05 00:34:08 +0900

    monster slayer update
    combine methods

--- vue/udemy-vue-the-complete-guide/project/monster-slayer-time-ver/app.js
+++ vue/udemy-vue-the-complete-guide/project/monster-slayer-time-ver/app.js
@@ -5,52 +5,81 @@ function randomNum(max, min){
 const app = Vue.createApp({
     data(){
         return{
-            controlView: false,
+            showControler: false,
             monsterHP: 1000,
             userHP: 1000,
-            remaningTime: 3,
+            timer: 10,
             result: null,
-            logRecords:[]
+            logRecords:[],
+            attackInterval: null,
+            timerInterval: null
         }
     },
     watch: {
-        remaningTime(){
-            if(this.remaningTime < 0){
-                this.controlView = false;
+        timer(){
+            if(this.timer === 0){
                 this.result = 'timeover';
+                this.gameStop();
+                
             }
         },
+        monsterHP(){
+            if(this.monsterHP === 0 & this.userHP !== 0){
+                this.gameStop();
+                this.result = 'win';
+                
+            }else if(this.monsterHP === 0 & this.userHP === 0){
+                this.result = 'draw'
+            }
+        },
+        userHP(){
+            if(this.userHP === 0 & this.monsterHP !== 0){
+                this.result = 'lose';
+                this.gameStop();
+            }else if(this.monsterHP === 0 & this.userHP === 0){
+                this.result = 'draw'
+            }
+        }
     },
     computed: {
-        gameStart(){
-            if(this.monsterHP === 0 || this.userHP === 0){
-                this.monsterHP= 1000,
-                this.userHP= 1000,
-                this.remaningTime= 10,
-                this.result= null,
-                this.logRecords=[]
-            }
-            if(this.remaningTime !== 0){
-                const mosterloop= setInterval(() => this.attackUser(), 1000);
-            }else{
-                clearInterval(mosterloop);
-            }
-        },
+        
     },
     methods: {
         gameBtn(){
-            this.controlView = !this.controlView;
-            if(this.controlView){
-                this.countdown();
-                this.gameStart();
+            this.showControler = !this.showControler;
+            this.gameStart();
+        },
+        gameStart(){
+            this.monsterHP= 1000;
+            this.userHP= 1000;
+            this.timer= 10;
+            this.result= null;
+            this.logRecords=[];
+            this.timerInterval = setInterval(() => this.timer -= 1, 1000);
+            this.attackInterval = setInterval(() => this.attackUser(), randomNum(700,500));
+            },
+        gameStop(){
+            clearInterval(this.timerInterval);
+            clearInterval(this.attackInterval);
+            this.timerInterval = null;
+            this.attackInterval = null;
+        },
+        attackUser(){
+            const val = randomNum(150,50);
+            if(this.userHP - val <= 0){
+                this.userHP = 0;
+            }else{
+                this.userHP -= val;
+                this.logRecord("Monster","attack","you",val);
             }
         },
-        countdown(){
-            if(this.remaningTime !== 0){
-                const remane = setInterval(() => this.remaningTime -= 1, 1000);
-                if(this.remaningTime === 0){
-                    clearInterval(remane);
-                }
+        attackMonster(){
+            const val = randomNum(30,10);
+            if(this.monsterHP - val <= 0){
+                this.monsterHP = 0;
+            }else{
+                this.monsterHP -= val;
+                this.logRecord("You","attack","monster",val);
             }
         },
         logRecord(performer, action, target, value){
@@ -61,16 +90,6 @@ const app = Vue.createApp({
                 val: value
             };
             this.logRecords.unshift(log);
-        },
-        attackUser(){
-            const val = randomNum(100,10);
-            this.userHP -= val;
-            this.logRecord("Monster","attack","you",val);
-        },
-        attackMonster(){
-            const val = randomNum(30,10);
-            this.monsterHP -= val;
-            this.logRecord("You","attack","monster",val);
         },
     }
 });
