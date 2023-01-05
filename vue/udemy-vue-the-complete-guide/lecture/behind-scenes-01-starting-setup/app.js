a481c769e04267de5c391fe7500ddf1432ce61a8
Sun-mie <104572619+Sun-mie@users.noreply.github.com>
2023-01-05 22:02:25 +0900

    Vue.js life cycle

--- vue/udemy-vue-the-complete-guide/lecture/behind-scenes-01-starting-setup/app.js
+++ vue/udemy-vue-the-complete-guide/lecture/behind-scenes-01-starting-setup/app.js
@@ -13,6 +13,37 @@ const app = Vue.createApp({
       this.message = this.currentUserInput;
     },
   },
+  //크롬 개발자 모드에서 디버깅하며 살펴보기
+  //생성에관한 주기
+  beforeCreate(){
+    console.log('beforeCreated()');
+  },
+  created(){
+    console.log('created()');
+  },
+  beforeMount(){
+    console.log('boforeMount()');
+  },
+  mounted(){
+    console.log('mounted()');
+  },
+  //업데이트에 관한 주기
+  beforeUpdate(){
+    console.log('beforeUpdate()');
+  },
+  updated(){
+    console.log('updated()');
+  },
+  //unmount 주기
+  beforeUnmount(){
+    console.log('beforeUnmount()');
+  },
+  unmount(){
+    console.log('unmount()');
+  }
 });
-
 app.mount('#app');
+//실제 애플리케이션에선 아래 메서드를 호출할 일이 거의 없음.
+//unmount 주기를 보여주기 위한 메서드
+setTimeout(()=> {app.unmount();},3000)
+
