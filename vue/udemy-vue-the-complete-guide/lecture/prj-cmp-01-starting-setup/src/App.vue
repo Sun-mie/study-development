27cb08f741c7970e16044eb33e807d2c3dc57d75
Sun-mie <104572619+Sun-mie@users.noreply.github.com>
2023-01-30 23:20:40 +0900

    lecture project practice

---
+++ vue/udemy-vue-the-complete-guide/lecture/prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <ul>
+    <stored-resources :resources="storedResources">
+    </stored-resources>
+  </ul>
+</template>
+
+<script>
+import StoredResources from './components/learning-resources/StoredResources.vue';
+
+export default {
+  components:{
+    StoredResources
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation.',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.org'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+/* App.vue 에서 모든 스타일이 전역으로 등록됨*/
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
